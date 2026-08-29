e0aa41014efca7315618dfd1dd80001131441fc9
JB Mccallaghan <JosephM@agilebridge.co.za>
2026-08-29 13:01:53 +0200

    feat: add login page object

---
+++ src/projects/contact-list/pages/LoginPage.ts
@@ -0,0 +1,25 @@
+import type { Locator, Page } from '@playwright/test';
+
+export class LoginPage {
+  readonly emailField: Locator;
+  readonly passwordField: Locator;
+  readonly submitButton: Locator;
+  readonly errorMessage: Locator;
+
+  constructor(private readonly page: Page) {
+    this.emailField = page.getByPlaceholder('Email');
+    this.passwordField = page.getByPlaceholder('Password');
+    this.submitButton = page.getByRole('button', { name: 'Submit' });
+    this.errorMessage = page.locator('#error');
+  }
+
+  async navigate(): Promise<void> {
+    await this.page.goto('/');
+  }
+
+  async login(email: string, password: string): Promise<void> {
+    await this.emailField.fill(email);
+    await this.passwordField.fill(password);
+    await this.submitButton.click();
+  }
+}
